a6d087925607ae82fdfb2ea03ea37b389d2f8b5e
Perry E. Henderson <hendersonperry96@yahoo.com>
2017-06-30 14:43:37 -0400

    first commit

---
+++ basic.py
@@ -0,0 +1,7 @@
+name = input('Please tell me your name:')
+
+print('Hello' + name, end = '')
+
+print('Welcome to the new adventure of Code.')
+
+print('This will be an awesome experience' + name + '!')
